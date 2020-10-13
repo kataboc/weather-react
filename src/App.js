3d25bf20c9b2df93ec3c428a6ebb5b40ba58eb26
kataboc <katte94@gmail.com>
2020-10-13 14:39:05 +0200

    installed extensions
    Removed Head.js
    Installed axios and Bootsrtap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import "./App.css";
-import Head from "./Head";
 import Body from "./Body";
 
 export default function App() {
   return (
     <div>
-      <Head />
       <Body />
       <p>
         Coded by{" "}
